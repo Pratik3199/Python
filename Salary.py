faa54669951f63db0d2c167953e37f3fbe21a456
Pratik <34038791+Pratik3199@users.noreply.github.com>
2018-08-14 12:28:27 +0530

    Create Salary.py

---
+++ Salary.py
@@ -0,0 +1,9 @@
+#Pratik Singh
+
+salary=float(input("Enter your salary"))
+new_salary=salary*2.62
+print(new_salary)
+if(new_salary > 100000000000):
+    print("Give me some money please")
+else:
+    print("Take some money from me")
